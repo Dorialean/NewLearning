d11769b82a136b323542fe950b380689d3a8d322
Dorialean <lana.lakutina@mail.ru>
2022-02-23 17:48:53 +0300

    another one

--- LeetCodeExercises.cs
+++ LeetCodeExercises.cs
@@ -50,8 +50,96 @@ namespace NewLearning
             int CountNums(int n, int[] nums) => nums.Where(x => x == n).Count();
         }
 
-        
+        //Представить в виде Char массива , складывать умножая на кол-во букв в алфавите.
+        public int TitleToNumber(string columnTitle)
+        {
+            const int ASCII_USELESS_NUMS = 64;
+            char[] columnCharTitle = columnTitle.ToCharArray();
+            int sum = 0;
+            int i = columnCharTitle.Length - 1;
+            foreach (char ch in columnCharTitle)
+            {
+                sum += (ch - ASCII_USELESS_NUMS) * (int)Math.Pow(26, i);
+                i--;
+            }
+            return sum;
+        }
+        //Поиск перебором с двух сторон
+        public int[] TwoSum(int[] nums, int target)
+        {
+            int[] indexRes = new int[2];
+            for (int i = 0; i < nums.Length; i++)
+            {
+                for (int j = nums.Length-1; j > 0 ; j--)
+                {
+                    if (i == j) continue;
+                    if (nums[i] + nums[j] == target) 
+                    {
+                        indexRes[0] = i;
+                        indexRes[1] = j;
+                        return indexRes;
+                    }
+                }
+            }
+            return indexRes;
+        }
 
+        //Очередь для прохождения по графу, dict для результата.
+        public class Node
+        {
+            public int val;
+            public IList<Node> neighbors;
+
+            public Node()
+            {
+                val = 0;
+                neighbors = new List<Node>();
+            }
+
+            public Node(int _val)
+            {
+                val = _val;
+                neighbors = new List<Node>();
+            }
+
+            public Node(int _val, List<Node> _neighbors)
+            {
+                val = _val;
+                neighbors = _neighbors;
+            }
+        }
+        
+        public Node CloneGraph(Node node)
+        {
+            Queue<Node> q = new Queue<Node>();
+            Dictionary<Node, Node> dict = new Dictionary<Node, Node>();
+
+            if (node == null)
+            {
+                return null;
+            }
+
+            q.Enqueue(node);
+            dict.Add(node, new Node(node.val));
+
+            while (q.Count > 0)
+            {
+                Node cur = q.Dequeue();
+
+                foreach (var nei in cur.neighbors)
+                {
+                    if (!dict.ContainsKey(nei))
+                    {
+                        dict.Add(nei, new Node(nei.val));
+                        q.Enqueue(nei);
+                    }
+
+                    dict[cur].neighbors.Add(dict[nei]);
+                }
+            }
+
+            return dict[node];
+        }
     }
 }
 
--- Program.cs
+++ Program.cs
@@ -6,8 +6,15 @@ Algorithms algorithms = new Algorithms();
 LeetCodeExercises leetCode = new LeetCodeExercises();
 CodeWarsExercises codeWars = new CodeWarsExercises();
 
-leetCode.Subsets(new int[] { 1, 2,3,4 });
-Console.WriteLine(leetCode.MajorityElement(new int[] { 6, 5, 5 }) );
+LeetCodeExercises.Node nd = new LeetCodeExercises.Node(1);
+nd.neighbors.Add(new LeetCodeExercises.Node(2));
+nd.neighbors.Add(new LeetCodeExercises.Node(3));
+nd.neighbors[1].neighbors.Add(new LeetCodeExercises.Node(4));
+
+var resNode = leetCode.CloneGraph(nd);
+
+
+
 
 
 
