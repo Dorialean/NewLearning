345218e94f0dab4b9fbe5821755f8bb50916758f
Dorialean <lana.lakutina@mail.ru>
2022-02-25 23:26:43 +0300

    another one exersice

--- Program.cs
+++ Program.cs
@@ -6,13 +6,6 @@ Algorithms algorithms = new Algorithms();
 LeetCodeExercises leetCode = new LeetCodeExercises();
 CodeWarsExercises codeWars = new CodeWarsExercises();
 
-LeetCodeExercises.Node nd = new LeetCodeExercises.Node(1);
-nd.neighbors.Add(new LeetCodeExercises.Node(2));
-nd.neighbors.Add(new LeetCodeExercises.Node(3));
-nd.neighbors[1].neighbors.Add(new LeetCodeExercises.Node(4));
-
-var resNode = leetCode.CloneGraph(nd);
-
 
 
 
