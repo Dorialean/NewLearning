7a24ed1b549b0437d6f5f09f54774be3baab138d
Dorialean <lana.lakutina@mail.ru>
2022-04-02 19:32:23 +0300

    another leetcode

--- Program.cs
+++ Program.cs
@@ -6,16 +6,6 @@ Algorithms algorithms = new Algorithms();
 LeetCodeExercises leetCode = new LeetCodeExercises();
 CodeWarsExercises codeWars = new CodeWarsExercises();
 
-LeetCodeExercises.ListNode l3 = new LeetCodeExercises.ListNode(3);
-LeetCodeExercises.ListNode l2 = new LeetCodeExercises.ListNode(4, l3);
-LeetCodeExercises.ListNode l1 = new LeetCodeExercises.ListNode(2, l2);
-
-LeetCodeExercises.ListNode l6 = new LeetCodeExercises.ListNode(4);
-LeetCodeExercises.ListNode l5 = new LeetCodeExercises.ListNode(6, l6);
-LeetCodeExercises.ListNode l4 = new LeetCodeExercises.ListNode(5, l5);
-
-Console.WriteLine(leetCode.AddTwoNumbers(l1,l4));
-
 
 
 
