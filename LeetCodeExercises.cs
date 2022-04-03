b25936ab05f984b65417cdbebbfd583d8c523b49
Dorialean <lana.lakutina@mail.ru>
2022-04-03 22:31:22 +0300

    This exercise i hate

--- LeetCodeExercises.cs
+++ LeetCodeExercises.cs
@@ -289,6 +289,36 @@ namespace NewLearning
             }
             return false;
         }
+
+        public void NextPermutation(int[] nums)
+        {
+            int i = nums.Length - 2;
+            while (i >= 0 && nums[i + 1] <= nums[i]) i--;
+            if(i >= 0)
+            {
+                int j = nums.Length - 1;
+                while(nums[j] <= nums[i]) j--;
+                swap(nums, i, j);
+            }
+            reverse(nums, i + 1);
+        }
+
+        private void reverse(int[] nums, int start)
+        {
+            int i = start, j = nums.Length-1;
+            while (i < j)
+            {
+                swap(nums, i, j);
+                i++;j--;
+            }
+        }
+
+        private void swap(int[] nums, int i, int j)
+        {
+            int t = nums[i];
+            nums[i] = nums[j];
+            nums[j] = t;
+        }
     }
 }
 
