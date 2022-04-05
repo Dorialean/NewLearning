1dd23fa731790510edfd1b86e5d941cac74f19f2
Dorialean <lana.lakutina@mail.ru>
2022-04-05 12:16:12 +0300

    LengthOfLongestSubstring  should recreate it with LINQ

--- LeetCodeExercises.cs
+++ LeetCodeExercises.cs
@@ -338,6 +338,24 @@ namespace NewLearning
             }
             return dummy.next;
         }
+
+        public int LengthOfLongestSubstring(string s)
+        {
+            if (s == null || s == String.Empty) return 0;
+
+            HashSet<char> set = new HashSet<char>();
+            int currentMax = 0, i = 0, j = 0;
+
+            while (j < s.Length)
+                if (!set.Contains(s[j]))
+                {
+                    set.Add(s[j++]);
+                    currentMax = Math.Max(currentMax, j - i);
+                }
+                else set.Remove(s[i++]);
+
+            return currentMax;
+        }
     }
 }
 
