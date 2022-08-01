63bc84a0e9e6b1cf05af0c27e0d5ee1003032a24
Dorialean <lana.lakutina@mail.ru>
2022-08-01 20:30:08 +0300

    UniquePaths

--- LeetCodeExercises.cs
+++ LeetCodeExercises.cs
@@ -696,7 +696,35 @@ namespace NewLearning
         }
 
         public bool IsAnagram(string s, string t) => string.Concat(s.OrderBy(c => c)) == string.Concat(t.OrderBy(c => c));
+
         
+        public int UniquePaths(int m, int n)
+        {
+            int[,] matrix = new int[m,n];
+            matrix = FillWithOnes(matrix);
+
+            for (int i = 1; i <= matrix.GetLength(0) - 1; i++)
+                for (int j = 1; j <= matrix.GetLength(1) - 1; j++)
+                    matrix[i, j] = matrix[i - 1, j] + matrix[i, j - 1];
+
+            return matrix[m - 1, n - 1];
+        }
+
+        private int[,] FillWithOnes(int[,] matrix)
+        {
+            int m = matrix.GetLength(0);
+            int n = matrix.GetLength(1);
+
+            matrix[0, 0] = 1;
+            if (m > 1)
+                for (int i = 1; i <= matrix.GetLength(0) - 1; i++)
+                    matrix[i, 0] = 1;
+            if (n > 1)
+                for (int i = 1; i <= matrix.GetLength(1) - 1; i++)
+                    matrix[0, i] = 1;
+
+            return matrix;
+        }
 
 
     }
